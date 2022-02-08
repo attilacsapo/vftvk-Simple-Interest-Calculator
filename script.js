be55eed815c05fe47c0e5364d357d737fc2e9793
attilacsapo <attila.csapo@chanceindia.org>
2022-02-08 19:40:24 +0100

    Ibm cloud project 1

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    e = p*(r/100)*y;
+    currentDate = new Date();
+    currentYear = currentDate.getFullYear();
+    futureYear =  currentYear + parseInt(y);
+
+    document.getElementById("resultPrincipal").innerHTML = "If you deposit " + p;
+    document.getElementById("resultRate").innerHTML = "at an interest rate of " + r + "%,";
+    document.getElementById("resultEarn").innerHTML = "You will receive " + e.toFixed(2);
+    document.getElementById("resultYears").innerHTML = "in the year " + futureYear + ".";
+
+        return e
     
 }
-        
+
